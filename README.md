219381e837e171e51f5a2e186a8ba9d80f061ffb
shoooya <shoooya17@gmail.com>
2023-03-28 19:20:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# 初回のみ
+## vscode extensionインストール
+```:ホストで実行
+$ sh init.sh
+```
+
+## flutterプロジェクト作成
+```:flutterコンテナ内で実行
+$ flutter config --enable-web
+$ flutter create .
+```
---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+code --install-extension dart-code.flutter
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+# export DOCKER_USER_ID=`id -u`
+# export DOCKER_GROUP_NAME=`id -un`
+# export DOCKER_GROUP_ID=`id -g`
